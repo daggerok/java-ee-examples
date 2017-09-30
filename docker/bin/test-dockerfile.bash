dd9f097dd5556c7572264084ddf021f2338cfc1b
Maksim Kostromin <daggerok@gmail.com>
2017-09-30 14:06:51 +0300

    Initial commit

---
+++ docker/bin/test-dockerfile.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -x
+docker build --force-rm -f ./docker/src/Dockerfile -t docker-java-ee-example .
+docker run -d -p 8080:8080 --rm --name app docker-java-ee-example
+sleep 20
+http :8080
+docker stop app
+
+bash docker/bin/cleanup.bash
---
+++ src/main/java/daggerok/service/AppService.java
@@ -0,0 +1,21 @@
+package daggerok.service;
+
+import javax.ejb.Singleton;
+import javax.ejb.Stateless;
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@Singleton
+@Stateless
+@ApplicationScoped
+public class AppService {
+  public List<String> getResult() {
+    return asList(
+        "one",
+        "two",
+        "three"
+    );
+  }
+}
