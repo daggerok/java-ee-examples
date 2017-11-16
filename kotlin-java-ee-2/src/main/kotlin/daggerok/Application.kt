543346d6a2741ab3fa3b8d88dfeb4c4d3d3daef0
Maksim Kostromin <daggerok@gmail.com>
2017-11-16 05:44:09 +0200

    add bash_functions for wait and kill on port; init kotlin-java-ee-2

---
+++ kotlin-java-ee-2/src/main/kotlin/daggerok/Application.kt
@@ -0,0 +1,51 @@
+package daggerok
+
+import javax.ejb.Stateless
+import javax.enterprise.context.ApplicationScoped
+import javax.inject.Inject
+import javax.json.Json
+import javax.ws.rs.*
+import javax.ws.rs.core.Application
+import javax.ws.rs.core.MediaType.APPLICATION_JSON
+
+interface JavaEeService {
+  fun hi(name: String): String
+}
+
+@Stateless
+@ApplicationScoped
+open class JavaEeServiceImpl : JavaEeService {
+  override fun hi(name: String) = "hello, $name!"
+}
+
+@Path("")
+@Produces(APPLICATION_JSON)
+open class JavaEeResource {
+
+  @Inject
+  private lateinit var javaEeService: JavaEeService
+
+  @GET
+  open fun hiAll() = hiOne("world")
+
+  @GET
+  @Path("{name}")
+  open fun hiOne(@PathParam("name") name: String) =
+      Json.createObjectBuilder()
+          .add("data", javaEeService.hi(name))
+          .build()
+}
+
+@ApplicationScoped
+@ApplicationPath("")
+open class RestApplication : Application() {
+  private val classes = setOf(
+      JavaEeResource::class.java
+  )
+  override fun getClasses() = classes
+
+  private val singletons = setOf(
+      JavaEeServiceImpl()
+  )
+  override fun getSingletons() = singletons
+}
