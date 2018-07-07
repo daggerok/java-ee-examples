3c15d20a342f13b482a7f7556b3ce6e7dfc2ce7d
Maksim Kostromin <daggerok@gmail.com>
2018-07-07 16:05:46 +0300

    if you're not using et, fuck off (C) whatthecommit.com

--- jax-rs-regex-path/src/main/java/daggerok/App.java
+++ jax-rs-regex-path/src/main/java/daggerok/App.java
@@ -4,8 +4,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-//tag::content[]
 @ApplicationScoped
 @ApplicationPath("")
 public class App extends Application { }
-//end::content[]
--- jax-rs-regex-path/src/main/java/daggerok/health/HealthResource.java
+++ jax-rs-regex-path/src/main/java/daggerok/health/HealthResource.java
@@ -1,23 +1,33 @@
 package daggerok.health;
 
-import daggerok.app.AppResource;
+import io.vavr.collection.HashMap;
 
 import javax.ejb.Stateless;
 import javax.json.Json;
+import javax.validation.constraints.Size;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
+//tag::content[]
 @Stateless
 @Path("api")
 @Produces(APPLICATION_JSON)
 public class HealthResource {
 
+  static final Map<String, String> pingPongMap
+      = HashMap.of("ping", "pong",
+                   "pong", "ping")
+               .toJavaMap();
+
+  //end::content[]
   @Context
   UriInfo uriInfo;
 
@@ -25,41 +35,29 @@ public class HealthResource {
   @Path("")
   public Response index() {
     return Response.ok(Json.createObjectBuilder()
-                           .add("ping", uriInfo.getBaseUriBuilder()
-                                                .path(AppResource.class)
-                                                .path(AppResource.class, "pingPong")
-                                                .build("ping")
-                                                .toString())
-                           .add("pong", uriInfo.getBaseUriBuilder()
-                                                .path(AppResource.class)
-                                                .path(AppResource.class, "pingPong")
-                                                .build("pong")
-                                                .toString())
-                           .add("health", uriInfo.getBaseUriBuilder()
-                                                .path(HealthResource.class)
-                                                .path(HealthResource.class, "health")
-                                                .build()
-                                                .toString())
-                           .add("_self", uriInfo.getBaseUriBuilder()
-                                                .path(HealthResource.class)
-                                                .path(HealthResource.class, "index")
-                                                .build()
-                                                .toString())
+                           .add("health", linkTo("health", "health"))
+                           .add("ping", linkTo("health", "ping"))
+                           .add("pong", linkTo("health", "pong"))
                            .build())
                    .build();
   }
 
+  String linkTo(@Size(min = 1) final String... args) {
+    return uriInfo.getBaseUriBuilder()
+                  .path(HealthResource.class)
+                  .path(HealthResource.class, args[0])
+                  .build(args[1])
+                  .toString();
+  }
+
+  //tag::content[]
   @GET
-  @Path("health")
-  public Response health() {
+  @Path("{path: (health|ping|pong)}")
+  public Response health(@PathParam("path") final String path) {
     return Response.ok(Json.createObjectBuilder()
-                           .add("status", "UP")
-                           .add("_self", uriInfo.getBaseUriBuilder()
-                                                .path(HealthResource.class)
-                                                .path(HealthResource.class, "health")
-                                                .build()
-                                                .toString())
+                           .add("status", pingPongMap.getOrDefault(path, "UP"))
                            .build())
                    .build();
   }
 }
+//end::content[]
